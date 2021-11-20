fe0ca6a1587e27bb81b98c663765256f3047726d
tenznz <dhugkar@gmail.com>
2021-11-21 04:02:53 +0530

    Added: Code for search value

--- src/com/bridgelabz/bst/MyBinaryTreeTest.java
+++ src/com/bridgelabz/bst/MyBinaryTreeTest.java
@@ -6,29 +6,32 @@ import org.junit.Test;
 public class MyBinaryTreeTest {
 
 	@Test
-	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+	public void givenABinaryTree_WhenAddingElements_ThenTreeContainsThoseElements() {
 		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
 		binaryTree.add(56);
 
 		binaryTree.add(30);
 		binaryTree.add(70);
-		
+
 		binaryTree.add(22);
 		binaryTree.add(40);
 		binaryTree.add(60);
-		
+
 		binaryTree.add(95);
 		binaryTree.add(11);
 		binaryTree.add(65);
-		
+
 		binaryTree.add(3);
 		binaryTree.add(16);
 		binaryTree.add(63);
-		
+
 		binaryTree.add(67);
 		binaryTree.inOrder();
 		int size = binaryTree.getSize();
 		Assert.assertEquals(13, size);
-		System.out.print("Size of tree is " + size);
+		System.out.println("Size of tree is " + size);
+		Boolean check = binaryTree.search(63);
+		Assert.assertTrue(check);
+		System.out.print("63 -> " + check);
 	}
 }
